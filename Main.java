cddbe133199f6da3b88ebee4075e074540bede04
Sacha Lhopital <sacha.lhopital@etu.univ-lyon1.fr>
2017-10-03 16:34:20 +0200

    Changement mineur

--- Main.java
+++ Main.java
@@ -8,30 +8,11 @@ public class Main {
         Agent agent = new Agent(motivation);
         Environment env = new Environment();
 
-        /*Experience e1 = new Experience("e1");
-        Experience e2 = new Experience("e2");
-
-        Result r1 = new Result("r1");
-        Result r2 = new Result("r2");
-        Result r = null;
-
-        agent.addPossibility(e1);
-        agent.addPossibility(e2);*/
-
-        /*Agent.Interaction i11 = new Agent.Interaction(e1, r1);
-        Agent.Interaction i12 = new Agent.Interaction(e1, r2);
-        Agent.Interaction i21 = new Agent.Interaction(e2, r1);
-        Agent.Interaction i22 = new Agent.Interaction(e2, r2);*/
-
-        /*env.addRule1(e1, r1, 1);
-        env.addRule2(e1, r2, -1);
-        env.addRule1(e2, r1, 1);
-        env.addRule2(e2, r2, -1);*/
-
         while(count != 50) {
 
             String action = agent.chooseExperience();
             int r = env.getResult(action);
+            
 
             System.out.println("DO : " + action.toString());
             System.out.println("RESULT Environnement " + r);
